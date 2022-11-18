0b0974962c038cc058b7d0716548cfc51e702840
fdervisi <fdervisi@googlemail.com>
2022-11-18 14:16:42 +0000

    dd

--- 5-TFCDK-simple-flat-structure/main.ts
+++ 5-TFCDK-simple-flat-structure/main.ts
@@ -3,8 +3,9 @@
 import { Construct } from 'constructs';
 import { App, TerraformOutput, TerraformStack } from 'cdktf';
 import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
+import { AzurermProvider } from '@cdktf/provider-azurerm/lib/provider';
 import { Vpc } from '@cdktf/provider-aws/lib/vpc';
-import { Subnet } from '@cdktf/provider-aws/lib/subnet';
+import { Subnet as AwsSubnet } from '@cdktf/provider-aws/lib/subnet';
 import { InternetGateway } from '@cdktf/provider-aws/lib/internet-gateway';
 import { RouteTable } from '@cdktf/provider-aws/lib/route-table';
 import { RouteTableAssociation } from '@cdktf/provider-aws/lib/route-table-association';
@@ -12,11 +13,16 @@ import { Route } from '@cdktf/provider-aws/lib/route';
 import { DataAwsAmi } from '@cdktf/provider-aws/lib/data-aws-ami';
 import { SecurityGroup } from '@cdktf/provider-aws/lib/security-group';
 import { Instance } from '@cdktf/provider-aws/lib/instance';
+import { ResourceGroup } from '@cdktf/provider-azurerm/lib/resource-group';
+import { VirtualNetwork } from '@cdktf/provider-azurerm/lib/virtual-network';
+import { LinuxVirtualMachine } from '@cdktf/provider-azurerm/lib/linux-virtual-machine';
+import { Subnet } from '@cdktf/provider-azurerm/lib/subnet';
+import { NetworkInterface } from '@cdktf/provider-azurerm/lib/network-interface';
 
-class MyStack extends TerraformStack {
+class MyAwsStack extends TerraformStack {
   region: any;
   vpc: Vpc;
-  subnet: Subnet;
+  subnet: AwsSubnet;
   igw: InternetGateway;
   rt: RouteTable;
   rt_association: RouteTableAssociation;
@@ -40,7 +46,7 @@ class MyStack extends TerraformStack {
     });
 
     // Create a Subnet
-    this.subnet = new Subnet(this, 'subnet_1', {
+    this.subnet = new AwsSubnet(this, 'subnet_1', {
       cidrBlock: '10.0.0.0/24',
       vpcId: this.vpc.id,
       tags: { Name: 'subnet_1' },
@@ -111,6 +117,75 @@ class MyStack extends TerraformStack {
   }
 }
 
+class MyAzureStack extends TerraformStack {
+  rg: ResourceGroup;
+  vnet: VirtualNetwork;
+  subnet: Subnet;
+  network_interface: NetworkInterface;
+  vm: LinuxVirtualMachine;
+
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new AzurermProvider(this, 'AzureRm', {
+      features: {
+        resourceGroup: { preventDeletionIfContainsResources: false },
+      },
+    });
+
+    // Create a Ressource Group
+    this.rg = new ResourceGroup(this, 'rg', {
+      name: 'fdervisi_IaC_basic',
+      location: 'North Europe',
+    });
+
+    // Create a VNet
+    this.vnet = new VirtualNetwork(this, 'vnet_1', {
+      resourceGroupName: this.rg.name,
+      name: 'vnet_1',
+      location: this.rg.location,
+      addressSpace: ['10.0.0.0/16'],
+    });
+
+    // Create a Subnet
+    this.subnet = new Subnet(this, 'subnet', {
+      addressPrefixes: ['10.0.0.0/16'],
+      resourceGroupName: this.rg.name,
+      virtualNetworkName: this.vnet.name,
+      name: 'subnet_1',
+    });
+
+    // Create Network Interface
+    this.network_interface = new NetworkInterface(this, 'nic', {
+      name: 'nic_1',
+      location: this.rg.location,
+      resourceGroupName: this.rg.name,
+      ipConfiguration: [
+        {
+          name: 'nic_ip',
+          subnetId: this.subnet.id,
+          privateIpAddressAllocation: 'Dynamic',
+        },
+      ],
+    });
+
+    // Create a VM
+    this.vm = new LinuxVirtualMachine(this, 'vm', {
+      name: 'vm_1',
+      location: this.rg.location,
+      resourceGroupName: this.rg.name,
+      size: 'Standard_DS1_v2',
+      networkInterfaceIds: [this.network_interface.id],
+      adminUsername: 'Fatos',
+      adminPassword: 'Zscaler2022',
+      osDisk: [{ caching: 'ReadWrite', storageAccountType: 'Standard_LRS' }],
+      
+    });
+  }
+}
+
 const app = new App();
-new MyStack(app, '5-TFCDK-simple-flat-structure');
+new MyAwsStack(app, 'AwsStack');
+new MyAzureStack(app, 'AzureStack');
+
 app.synth();
