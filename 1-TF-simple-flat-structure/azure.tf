a0a0dba580c586ebd3fcb3e18239e7244aaa11f4
fdervisi <fdervisi@googlemail.com>
2022-11-18 11:12:31 +0000

    standart modules azure

--- 1-TF-simple-flat-structure/azure.tf
+++ 1-TF-simple-flat-structure/azure.tf
@@ -1,6 +1,10 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 # Create a Ressource Group
--- 4-TF-standard-modules/main.tf
+++ 4-TF-standard-modules/main.tf
@@ -1,10 +1,20 @@
 # Terraform configuration
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.0.0"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
     }
   }
 }
@@ -13,43 +23,61 @@ provider "aws" {
   region = "eu-south-1"
 }
 
+# Get Availability Zones Linux AMI
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# Get latest AWS Linux AMI
+data "aws_ami" "amazon-linux-2-kernel-5" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5*"]
+  }
+}
+
+# Create Networking
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.21.0"
 
-  azs = var.vpc_azs
-  name = "vpc_1"
-  cidr = "10.0.0.0/16"
-
-  private_subnets = ["10.0.0.0/24"]
-  public_subnets  = ["10.0.1.0/24"]
-
-  enable_nat_gateway = "true"
-
+  azs            = var.vpc_azs
+  name           = "vpc_1"
+  cidr           = "10.0.0.0/16"
+  public_subnets = ["10.0.0.0/24"]
   tags = {
-    Name        = "vpc_1"
-    Environment = "dev"
+    Name = "vpc_1"
   }
 }
 
+# Create Instance
 module "ec2_instances" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "2.12.0"
 
-  name           = "ec2_1"
-  instance_count = 1
-
-  ami                    = "ami-027379bcf3d69010d"
+  name                   = "ec2_1"
+  instance_count         = 1
+  ami                    = data.aws_ami.amazon-linux-2-kernel-5.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
-
   tags = {
-    Name        = "ec2_1"
-    Environment = "dev"
+    Name = "ec2_1"
   }
+}
+
+# Create a Ressource Group
+resource "azurerm_resource_group" "rg_iac" {
+  name     = "fdervisi_IaC_basic"
+  location = "North Europe"
+}
+
+
+module "vnet" {
+  source  = "Azure/vnet/azurerm"
+  version = "3.0.0"
+  resource_group_name = azurerm_resource_group.rg_iac.name
+  vnet_location = azurerm_resource_group.rg_iac.location
 }
